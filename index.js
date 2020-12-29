e0779464a3f819776f0cbcb92218068bff770e34
chinnychinchin <chinyan77@gmail.com>
2020-12-30 00:29:44 +0800

    added regex

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start((ctx) => ctx.reply("Welcome to Chins' fortune bot"))
 
-bot.hears('fortune', (ctx) => {
+bot.hears(new RegExp("fortune", "i"), (ctx) => {
 
     const [cookie] = xNumOfCookies(1)
     ctx.reply(cookie)
@@ -15,9 +15,18 @@ bot.hears('fortune', (ctx) => {
 
 bot.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
 
-    const numOfCookies = inlineQuery.query || 1;
-    const responseArray = xNumOfCookies(numOfCookies).map(cookie => {return {type: 'article', id: cookie.substring(20), title: cookie, input_message_content: {"message_text": cookie}}})
-    return answerInlineQuery(responseArray)
+    const numOfCookies = inlineQuery.query;
+    if(numOfCookies > 0 && numOfCookies < 11) {
+        
+        const responseArray = xNumOfCookies(numOfCookies).map(cookie => {return {type: 'article', id: cookie.substring(20), title: cookie, input_message_content: {"message_text": cookie}}})
+        return answerInlineQuery(responseArray)
+    }
+
+    else{
+        const responseArray = xNumOfCookies(1).map(cookie => {return {type: 'article', id: cookie.substring(20), title: cookie, input_message_content: {"message_text": cookie}}})
+        return answerInlineQuery(responseArray)
+    }
+    
   })
   
 
