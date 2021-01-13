29d71becee2ed3924a9360e526c23a2bbf5b6728
chinyanlim <chinyan77@gmail.com>
2021-01-13 22:02:24 +0800

    changed to article analyzer

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const {Telegraf, Markup} = require('telegraf');
+//Load required libraries
+const {Telegraf} = require('telegraf');
 const {xNumOfCookies} = require('./fortune-cookie');
-const {MenuTemplate, MenuMiddleware} = require('telegraf-inline-menu')
+const {MenuTemplate, MenuMiddleware} = require('telegraf-inline-menu');
+const fetch = require('node-fetch');
 
+//Create an instance of a bot
 const BOT_TOKEN = process.env.BOT_TOKEN
 const bot = new Telegraf(BOT_TOKEN)
 
 
+
+
+//Methods
 bot.hears(new RegExp("fortune", "i"), (ctx) => {
 
     const [cookie] = xNumOfCookies(1)
@@ -28,11 +34,34 @@ bot.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
     }
     
   })
+
+bot.command('analyze', async (ctx, next) => {
+
+  const offsetLength = ctx.message.entities[0].length
+  const articleContent = ctx.message.text.substring(offsetLength, ctx.message.text.length);
+  console.log(ctx.message)
+  if (!message.length){
+      //return menuMiddleware.replyToContext(ctx)
+      next(ctx)
+  }
+  else{
+
+      const result = await fetch('http://chinsfakebox.eastus.azurecontainer.io:8080/fakebox/check', {method: 'post', body: JSON.stringify({content: articleContent}), headers: { 'Content-Type': 'application/json' }})
+      const resultJson = await result.json()
+      ctx.reply(`Content score: ${resultJson['content']['score']} (${resultJson['content']['decision']})\n(Analysis by Veracity)`)
+
+  }
+  
+
+}, async (ctx) => { console.log('menu middleware'); await menuMiddleware.replyToContext(ctx, '') })
+
   
 
 //Inline keyboard menu
-const menuTemplate = new MenuTemplate(ctx => `Hi there! Welcome to Chinnnychinchin's fortune-cookie bot. Simply hit the button below to generate a fortune-cookie or send me a query like "fortune".`)
+const menuTemplate = new MenuTemplate(() => `Hi there! Welcome to Veracity. Simply send me a command /analyze + <YOUR ARTICLE> in the chatbox to analyze your article!`)
 
+//Buttons 
+/*
 menuTemplate.interact('Give me a cookie!', 'a', {
 do: async ctx => { 
     
@@ -42,6 +71,7 @@ do: async ctx => {
 
 }
 })
+*/
 
 const menuMiddleware = new MenuMiddleware('/', menuTemplate)
 bot.command('help', ctx => menuMiddleware.replyToContext(ctx))
@@ -50,7 +80,10 @@ bot.use(menuMiddleware)
 
 
 
-
+//To catch errors
+bot.catch((err, ctx) => {
+  console.log(`Ooops, encountered an error for ${ctx.updateType}`, err)
+})
 
 //Start bot
 const PORT = process.env.PORT
